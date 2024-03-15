b27e4b1732577ee789db113f20e598c4efa2fb5f
Tal Kohavy <talkohavy@gmail.com>
2024-03-15 14:26:54 +0200

    ErrorBoundary: removed scrollbar from ErrorTitle

--- apps/host/src/components/ErrorBoundaries/GlobalErrorBoundaryDevelopment/ErrorTitle.jsx
+++ apps/host/src/components/ErrorBoundaries/GlobalErrorBoundaryDevelopment/ErrorTitle.jsx
@@ -1,3 +1,3 @@
 export default function ErrorTitle({ error }) {
-  return <pre className='text-xl font-bold text-[#ff5655]'>{error.message}</pre>;
+  return <pre className='no-scrollbar w-full overflow-x-auto text-xl font-bold text-[#ff5655]'>{error.message}</pre>;
 }
