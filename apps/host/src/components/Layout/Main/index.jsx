a80e44942ea97e41d149908986cfa53621289d95
Tal Kohavy <talkohavy@gmail.com>
2024-03-15 23:33:12 +0200

    layout fixes

--- apps/host/src/components/Layout/Main/index.jsx
+++ apps/host/src/components/Layout/Main/index.jsx
@@ -1,3 +1,7 @@
 export default function Main({ children }) {
-  return <main className='flex size-full items-center justify-between bg-white dark:bg-[#383838]'>{children}</main>;
+  return (
+    <main className='flex h-full flex-grow items-center justify-between overflow-auto bg-white dark:bg-[#383838]'>
+      {children}
+    </main>
+  );
 }
