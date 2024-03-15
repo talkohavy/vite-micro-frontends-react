94afc5bdca5d5a0820f18ebff7eb5346aa04ecb2
Tal Kohavy <talkohavy@gmail.com>
2024-03-15 13:41:06 +0200

    updated debug.js

--- toolbox/debug.js
+++ toolbox/debug.js
@@ -1,21 +1,13 @@
-import { importJson } from './importJson.js';
 import { openTerminal } from 'open-terminal-programmatically';
 
-/** @typedef {import('open-terminal-programmatically').OpenTerminalProps} OpenTerminalProps */
-
-const vsCodeLaunchConfig = importJson('../.vscode/launch.json').configurations;
-
-function getLaunchScriptOf(scriptName) {
-  return vsCodeLaunchConfig.find(({ name }) => name === scriptName);
-}
-
-/** @type {Array<OpenTerminalProps>} */
+/** @type {Array<import('open-terminal-programmatically').OpenTerminalProps>} */
 const programs = [
   {
     isEncoded: true,
     config: {
       name: 'host',
       command: 'cd apps/host && npm run dev',
+      autoFocus: true,
     },
   },
   {
@@ -23,14 +15,8 @@ const programs = [
     config: {
       name: 'remote',
       command: 'cd apps/remote && npm run dev',
-      autoFocus: true,
     },
   },
-  // {
-  //   isDebugMode: true,
-  //   delayNextFor: 1000,
-  //   config: getLaunchScriptOf('some-script'),
-  // },
 ];
 
 async function runProgramInDebug() {
