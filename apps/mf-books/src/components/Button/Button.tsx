647d9dfa9533feda1db507ee195670060313c7ad
Tal Kohavy <talkohavy@gmail.com>
2024-11-15 01:45:16 +0200

    gave Button some styling

--- apps/mf-books/src/components/Button/Button.tsx
+++ apps/mf-books/src/components/Button/Button.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-import './Button.module.css';
+import styles from './Button.module.css';
 
 export default function Button() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <button type='button' className='shared-btn' onClick={() => setCount((s) => s + 1)}>
+      <button type='button' className={styles.sharedBtn} onClick={() => setCount((s) => s + 1)}>
         Click counter: {count}
       </button>
     </div>
