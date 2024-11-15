23357d7478cc30e9de38829d6fb315ddce190a84
Tal Kohavy <talkohavy@gmail.com>
2024-11-15 09:12:02 +0200

    small refactor to SideBar

--- apps/host/src/components/Layout/Sidebar/SideBar.tsx
+++ apps/host/src/components/Layout/Sidebar/SideBar.tsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+import DownArrow from '../../svgs/DownArrow';
 import SideBarLinkList from './SideBarLinkList';
 
 export default function Sidebar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
-    <div className='h-full w-80 shrink-0 bg-amber-100 p-3 shadow-md dark:bg-slate-500 sm:p-6'>
-      <SideBarLinkList />
-    </div>
+    <nav
+      className={clsx(
+        'relative flex h-full flex-col items-start justify-start bg-amber-50 p-10 shadow-md transition-all duration-300 dark:bg-slate-500',
+        isSidebarOpen ? 'w-xl' : 'w-4',
+      )}
+    >
+      <button
+        type='button'
+        onClick={() => setIsSidebarOpen((prev) => !prev)}
+        className='absolute right-0 top-1/2 size-8 translate-x-1/2 rounded-full bg-blue-400 p-2 opacity-20 hover:opacity-100'
+      >
+        <DownArrow className={clsx('stroke-black', isSidebarOpen ? 'rotate-90' : 'rotate-270')} />
+      </button>
+
+      {isSidebarOpen && <SideBarLinkList />}
+    </nav>
   );
 }
