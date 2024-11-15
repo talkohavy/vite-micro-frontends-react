628ffd2b64b9615474b30c8565d1edc6f7c26cbe
Tal Kohavy <talkohavy@gmail.com>
2024-11-15 09:08:28 +0200

    small refactor to Layout

--- apps/host/src/components/Layout/Layout.tsx
+++ apps/host/src/components/Layout/Layout.tsx
@@ -3,14 +3,16 @@ import Header from './Header';
 import Main from './Main';
 import Sidebar from './Sidebar';
 
-export default function Layout(props: PropsWithChildren) {
+type LayoutProps = PropsWithChildren;
+
+export default function Layout(props: LayoutProps) {
   const { children } = props;
 
   return (
     <div className='flex h-full flex-col items-start justify-start'>
       <Header />
 
-      <div className='flex size-full items-start justify-start'>
+      <div className='flex size-full items-center justify-center overflow-auto'>
         <Sidebar />
 
         <Main>{children}</Main>
