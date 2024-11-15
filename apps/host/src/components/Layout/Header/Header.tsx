eddc98454eff4ce2aad638956efa82aee9426c68
Tal Kohavy <talkohavy@gmail.com>
2024-11-15 09:09:13 +0200

    small refactor to Header

--- apps/host/src/components/Layout/Header/Header.tsx
+++ apps/host/src/components/Layout/Header/Header.tsx
@@ -2,7 +2,7 @@ import DarkModeToggle from '../../DarkModeToggle';
 
 export default function Header() {
   return (
-    <header className='z-20 flex h-12 w-full shrink-0 items-center justify-start bg-pink-300 shadow-sm dark:bg-gray-800 dark:shadow-dark-sm'>
+    <header className='relative flex h-15 w-full items-center justify-start bg-pink-200 shadow-sm dark:bg-gray-800 dark:shadow-dark-sm'>
       <DarkModeToggle size={20} />
     </header>
   );
