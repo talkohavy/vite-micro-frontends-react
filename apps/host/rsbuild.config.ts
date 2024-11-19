5dfcf227951e0162fc30537cd2a44dc9283c65eb
Tal Kohavy <talkohavy@gmail.com>
2024-11-19 09:13:29 +0200

    remove commented lines

--- apps/host/rsbuild.config.ts
+++ apps/host/rsbuild.config.ts
@@ -18,23 +18,3 @@ export default defineConfig({
     port: 3000,
   },
 });
-
-// import { defineConfig } from '@rsbuild/core';
-// import { pluginReact } from '@rsbuild/plugin-react';
-// import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
-
-// export default defineConfig({
-//   plugins: [
-//     pluginReact(),
-//     pluginModuleFederation({
-//       name: 'host',
-//       exposes: {
-//         './button': './src/button.tsx',
-//       },
-//       shared: ['react', 'react-dom'],
-//     }),
-//   ],
-//   server: {
-//     port: 3000,
-//   },
-// });
