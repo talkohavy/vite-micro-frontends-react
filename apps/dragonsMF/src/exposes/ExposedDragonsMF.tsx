27093d8b4cdcd105436a38b7e53095b1738ce064
Tal Kohavy <talkohavy@gmail.com>
2024-11-19 14:57:56 +0200

    removed unused MF

--- apps/dragonsMF/src/exposes/ExposedDragonsMF.tsx
+++
@@ -1,9 +0,0 @@
-import App from '../components/App';
-
-type ExposedDragonsMFProps = {
-  className?: string;
-};
-
-export default function ExposedDragonsMF(props: ExposedDragonsMFProps) {
-  return <App {...props} />;
-}
