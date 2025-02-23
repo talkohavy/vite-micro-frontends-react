3099fcba24e0cd5ac4366c23ac24c6a70ff20e72
Tal Kohavy <talkohavy@gmail.com>
2025-02-23 08:01:44 +0200

    converted the Header

--- apps/host/src/components/Layout/Header/Header.tsx
+++ apps/host/src/components/Layout/Header/Header.tsx
@@ -1,9 +1,13 @@
-import DarkModeToggle from '../../DarkModeToggle';
+import { ReactComponent as ViteIcon } from '../../../assets/vite2.svg';
+import DarkModeToggle from './DarkModeToggle';
+import styles from './Header.module.scss';
 
 export default function Header() {
   return (
-    <header className='relative flex h-15 w-full items-center justify-start bg-pink-200 shadow-sm dark:bg-gray-800 dark:shadow-dark-sm'>
-      <DarkModeToggle size={20} />
+    <header className={styles.header}>
+      <ViteIcon />
+
+      <DarkModeToggle />
     </header>
   );
 }
