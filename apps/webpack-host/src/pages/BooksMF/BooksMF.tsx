d8e40c39d5c42e3758fc92b7d69f08f355423ac2
Tal Kohavy <talkohavy@gmail.com>
2025-11-10 23:03:21 +0200

    BooksMF inside webpack tweaked

--- apps/webpack-host/src/pages/BooksMF/BooksMF.tsx
+++ apps/webpack-host/src/pages/BooksMF/BooksMF.tsx
@@ -1,13 +1,22 @@
-import { lazy, Suspense } from 'react';
+import { Suspense } from 'react';
+import { useFederatedComponent } from '@src/common/utils/useFederatedComponent';
+import { RemoteBundleType } from '@src/common/utils/useFederatedComponent/logic/constants';
 import MicroFrontendErrorBoundary from '@src/components/ErrorBoundaries/MicroFrontendErrorBoundary';
 
-const RemoteApp = lazy(() => import('mf_books/App'));
-
 export default function BooksMF() {
+  const { Component } = useFederatedComponent({
+    remoteName: 'mf_books',
+    moduleName: 'App',
+    remoteEntryUrl: 'http://localhost:3001/remoteEntry.js',
+    type: RemoteBundleType.Module,
+  });
+
+  if (!Component) return null;
+
   return (
     <MicroFrontendErrorBoundary>
-      <Suspense fallback={<div>loading...</div>}>
-        <RemoteApp />
+      <Suspense fallback='Loading books...'>
+        <Component />
       </Suspense>
     </MicroFrontendErrorBoundary>
   );
