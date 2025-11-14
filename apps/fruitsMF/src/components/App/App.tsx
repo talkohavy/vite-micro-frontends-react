1e83e65616f6c7e0d3173355fc42b88715122a48
Tal Kohavy <talkohavy@gmail.com>
2025-11-14 14:22:18 +0200

    styles fixed

--- apps/fruitsMF/src/components/App/App.tsx
+++ apps/fruitsMF/src/components/App/App.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx';
 import { ReactComponent as Cherry } from '../../assets/cherry.svg';
 import { ReactComponent as Grapes } from '../../assets/grapes.svg';
 import { ReactComponent as Strawberry } from '../../assets/strawberry.svg';
@@ -9,12 +8,10 @@ export default function App() {
   return (
     <div className={styles.app}>
       <div className={styles.icons}>
-        <img src='/vite.svg' className='logo' alt='Vite logo' />
-
-        <Cherry className={clsx(styles.logo, styles.react)} />
-        <Grapes className={clsx(styles.logo, styles.react)} />
-        <Strawberry className={clsx(styles.logo, styles.react)} />
-        <Watermelon className={clsx(styles.logo, styles.react)} />
+        <Cherry className={styles.logo} />
+        <Grapes className={styles.logo} />
+        <Strawberry className={styles.logo} />
+        <Watermelon className={styles.logo} />
       </div>
 
       <h1 className={styles.title}>Welcome from Fruits!</h1>
