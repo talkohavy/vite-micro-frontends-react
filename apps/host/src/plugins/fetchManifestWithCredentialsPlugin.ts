c22cfbfa58be81bf6841a74b57a2088ea352326f
Tal Kohavy <talkohavy@gmail.com>
2025-11-16 09:04:31 +0200

    fetchManifestWithCredentialsPlugin: added comment about when it applies

--- apps/host/src/plugins/fetchManifestWithCredentialsPlugin.ts
+++ apps/host/src/plugins/fetchManifestWithCredentialsPlugin.ts
@@ -1,5 +1,8 @@
 import { ModuleFederationRuntimePlugin } from '@module-federation/enhanced/runtime';
 
+/**
+ * This plugin is only applied when fetching the mf-manifest.json, not the remoteEntry.js
+ */
 export default function fetchManifestWithCredentialsPlugin(): ModuleFederationRuntimePlugin {
   return {
     name: 'fetch-manifest-with-credentials-plugin',
