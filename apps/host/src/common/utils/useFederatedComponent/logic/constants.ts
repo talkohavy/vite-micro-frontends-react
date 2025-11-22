018ec09fb597d09a4b990c53bc088f0553d8e90d
Tal Kohavy <talkohavy@gmail.com>
2025-11-22 15:25:13 +0200

    removed initedMicroFrontendsCache

--- apps/host/src/common/utils/useFederatedComponent/logic/constants.ts
+++ apps/host/src/common/utils/useFederatedComponent/logic/constants.ts
@@ -2,7 +2,6 @@ import { LazyExoticComponent } from 'react';
 import { FederatedModule } from '../types';
 
 export const componentsCache = new Map<string, LazyExoticComponent<FederatedModule>>();
-export const initedMicroFrontendsCache = new Map<string, boolean>();
 
 export const RemoteBundleTypes = {
   Module: 'module',
