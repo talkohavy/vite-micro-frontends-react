23368e3cbb5aec9715ff43c505efed62afb518fe
Tal Kohavy <talkohavy@gmail.com>
2026-03-10 23:07:45 +0200

    updated tailwind.config: index.html is inside src

--- apps/viteHost/tailwind.config.ts
+++ apps/viteHost/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const tailwindConfig: Config = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/index.html', './src/**/*.{js,ts,jsx,tsx}'],
   // safelist: [{ pattern: /^(bg-|border-|text-)/, variants: ['hover', 'active'] }, 'bg-red-200'],
   theme: {
     screens: { sm: '480px', md: '768px', lg: '976px', xl: '1440px' },
